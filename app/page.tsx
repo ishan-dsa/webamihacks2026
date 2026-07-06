c27e48f086e1a3b9502101d94540d64f1a8d49f5
ishan verma <ishanverma7976@gmail.com>
2026-07-06 13:13:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black">
+    <main className="min-h-screen bg-slate-50">
       <Navbar />
       <Hero />
       <About />
