c66a6982f8f8e1195573538b5479fb0a4e3b4907
ishan verma <ishanverma7976@gmail.com>
2026-07-06 17:12:11 +0530

    Update WhyParticipate.tsx

--- components/WhyParticipate.tsx
+++ components/WhyParticipate.tsx
@@ -91,7 +91,7 @@ export default function WhyParticipate() {
           transition={{ duration: 0.5 }}
           className="text-cyan-400 font-mono-code text-xs tracking-[0.3em] uppercase mb-4"
         >
-          &lt;/&gt; Why Participate
+     
         </motion.p>
         <motion.h2
           initial={{ opacity: 0, y: 20 }}
